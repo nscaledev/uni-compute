048135da7419c75af051192de4d2afa6acf61c69
Simon Murray <spjmurray@yahoo.co.uk>
2025-09-24 15:11:35 +0100

    Update Region API (#112)
    Use the new region API that treats security groups as a monolith as it's
    generally cleaner, easier to use, a lot less code!

--- pkg/provisioners/managers/cluster/securitygroup.go
+++ pkg/provisioners/managers/cluster/securitygroup.go
@@ -27,6 +27,7 @@ import (
 	"context"
 	"fmt"
 	"maps"
+	"reflect"
 	"slices"
 
 	"github.com/spjmurray/go-util/pkg/set"
@@ -90,6 +91,45 @@ func (p *Provisioner) securityGroupName(pool *unikornv1.ComputeClusterWorkloadPo
 	return fmt.Sprintf("%s-%s", p.cluster.Name, pool.Name)
 }
 
+// generateSecurityGroupRule generates a single security group rule.
+func generateRequiredSecurityGroupRule(in *unikornv1.FirewallRule, prefix unikornv1core.IPv4Prefix) *regionapi.SecurityGroupRule {
+	rule := &regionapi.SecurityGroupRule{
+		Direction: regionapi.NetworkDirection(in.Direction),
+		Protocol:  regionapi.NetworkProtocol(in.Protocol),
+		Cidr:      prefix.String(),
+	}
+
+	// TODO: Smell code.  I think the region controller should be responsible
+	// for managing CIDR handling.
+	if in.PortMax != nil {
+		rule.Port.Range = &regionapi.SecurityGroupRulePortRange{
+			Start: in.Port,
+			End:   *in.PortMax,
+		}
+	} else {
+		rule.Port.Number = &in.Port
+	}
+
+	return rule
+}
+
+// generateRequiredSecurityGroupRules creates all the security group rules we require based on
+// the input specification.  It essentially translates from our simple user facing API to that
+// employed by the region controller.
+func generateRequiredSecurityGroupRules(pool *unikornv1.ComputeClusterWorkloadPoolSpec) []regionapi.SecurityGroupRule {
+	out := make([]regionapi.SecurityGroupRule, 0, len(pool.Firewall))
+
+	for i := range pool.Firewall {
+		for _, prefix := range pool.Firewall[i].Prefixes {
+			rule := generateRequiredSecurityGroupRule(&pool.Firewall[i], prefix)
+
+			out = append(out, *rule)
+		}
+	}
+
+	return out
+}
+
 // generateSecurityGroup creates a new security group request.
 func (p *Provisioner) generateSecurityGroup(pool *unikornv1.ComputeClusterWorkloadPoolSpec) *regionapi.SecurityGroupWrite {
 	return &regionapi.SecurityGroupWrite{
@@ -97,6 +137,9 @@ func (p *Provisioner) generateSecurityGroup(pool *unikornv1.ComputeClusterWorklo
 			Name:        p.securityGroupName(pool),
 			Description: ptr.To("Security group for cluster " + p.cluster.Name),
 			Tags:        p.tags(pool),
+		},
+		Spec: regionapi.SecurityGroupSpec{
+			Rules: generateRequiredSecurityGroupRules(pool),
 		},
 	}
 }
@@ -167,16 +210,18 @@ func (p *Provisioner) reconcileSecurityGroups(ctx context.Context, client region
 		if err := securityGroups.add(poolName, securityGroup); err != nil {
 			return err
 		}
-
-		if err := p.reconcileSecurityGroupRules(ctx, client, poolName, securityGroup); err != nil {
-			return err
-		}
 	}
 
 	for poolName := range update.All() {
-		securityGroup := securityGroups[poolName]
+		currentSecurityGroup := securityGroups[poolName]
+		requiredSecurityGroup := required[poolName]
 
-		if err := p.reconcileSecurityGroupRules(ctx, client, poolName, securityGroup); err != nil {
+		// TODO: metadata e.g. tags etc.
+		if reflect.DeepEqual(currentSecurityGroup.Spec, requiredSecurityGroup.Spec) {
+			continue
+		}
+
+		if _, err := p.updateSecurityGroup(ctx, client, currentSecurityGroup.Metadata.Id, requiredSecurityGroup); err != nil {
 			return err
 		}
 	}
@@ -187,137 +232,6 @@ func (p *Provisioner) reconcileSecurityGroups(ctx context.Context, client region
 		log.Info("deleting security group", "pool", poolName, "id", securityGroup.Metadata.Id, "name", securityGroup.Metadata.Name)
 
 		if err := p.deleteSecurityGroup(ctx, client, securityGroup.Metadata.Id); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// securityGroupID generates a unqiue rule ID.  It either exists or it doesn't
-// there is no aliasing or need to update via the API.
-func securityGroupIDGenerate(in *regionapi.SecurityGroupRuleSpec) string {
-	id := fmt.Sprintf("%s-%s", in.Direction, in.Protocol)
-
-	if in.Port.Number != nil {
-		id += fmt.Sprintf("-%d", *in.Port.Number)
-	} else {
-		id += fmt.Sprintf("-%d-%d", in.Port.Range.Start, in.Port.Range.End)
-	}
-
-	id += fmt.Sprintf("-%s", in.Cidr)
-
-	return id
-}
-
-// generateRequiredSecurityGroupRule generates a single security group rule request.
-func (p *Provisioner) generateRequiredSecurityGroupRule(pool *unikornv1.ComputeClusterWorkloadPoolSpec, in *unikornv1.FirewallRule, prefix unikornv1core.IPv4Prefix) *regionapi.SecurityGroupRuleWrite {
-	rule := &regionapi.SecurityGroupRuleWrite{
-		Metadata: coreapi.ResourceWriteMetadata{
-			Name: "unikorn-compute-cluster-security-group",
-			Tags: p.tags(pool),
-		},
-		Spec: regionapi.SecurityGroupRuleSpec{
-			Direction: regionapi.NetworkDirection(in.Direction),
-			Protocol:  regionapi.NetworkProtocol(in.Protocol),
-			Cidr:      prefix.String(),
-		},
-	}
-
-	// TODO: Smell code.  I think the region controller should be responsible
-	// for managing CIDR handling.
-	if in.PortMax != nil {
-		rule.Spec.Port.Range = &regionapi.SecurityGroupRulePortRange{
-			Start: in.Port,
-			End:   *in.PortMax,
-		}
-	} else {
-		rule.Spec.Port.Number = &in.Port
-	}
-
-	return rule
-}
-
-// generateRequiredSecurityGroupRules creates all the security group rules we require based on
-// the input specification.  It essentially translates from our simple user facing API to that
-// employed by the region controller.
-func (p *Provisioner) generateRequiredSecurityGroupRules(pool *unikornv1.ComputeClusterWorkloadPoolSpec) map[string]*regionapi.SecurityGroupRuleWrite {
-	out := map[string]*regionapi.SecurityGroupRuleWrite{}
-
-	for i := range pool.Firewall {
-		for _, prefix := range pool.Firewall[i].Prefixes {
-			rule := p.generateRequiredSecurityGroupRule(pool, &pool.Firewall[i], prefix)
-
-			out[securityGroupIDGenerate(&rule.Spec)] = rule
-		}
-	}
-
-	return out
-}
-
-// securityGroupRuleIDMap is a set of security group rules for the cluster
-// indexed by security group ID (which is a unique tuple of direction, port range
-// and allowed address ranges).
-type securityGroupRuleIDMap map[string]*regionapi.SecurityGroupRuleRead
-
-// getSecurityGroupRuleIDMap gets a set of security group rules for the cluster
-// indexed by security group ID.
-func (p *Provisioner) getSecurityGroupRuleIDMap(ctx context.Context, client regionapi.ClientWithResponsesInterface, securityGroupID string) (securityGroupRuleIDMap, error) {
-	rules, err := p.listSecurityGroupRules(ctx, client, securityGroupID)
-	if err != nil {
-		return nil, err
-	}
-
-	out := securityGroupRuleIDMap{}
-
-	for i := range rules {
-		out[securityGroupIDGenerate(&rules[i].Spec)] = &rules[i]
-	}
-
-	return out, nil
-}
-
-// scheduleSecurityGroupRules does some simple boolean logic on the requested and existing
-// rule IDs to determine what needs to be created, and what needs to be deleted.
-func (p *Provisioner) scheduleSecurityGroupRules(provisioned map[string]*regionapi.SecurityGroupRuleRead, desired map[string]*regionapi.SecurityGroupRuleWrite) (set.Set[string], set.Set[string]) {
-	actualIDs := set.New[string](slices.Collect(maps.Keys(provisioned))...)
-	desiredIDs := set.New[string](slices.Collect(maps.Keys(desired))...)
-
-	return desiredIDs.Difference(actualIDs), actualIDs.Difference(desiredIDs)
-}
-
-// reconcileSecurityGroupRules creates and deletes rules for a security group
-// as needed.  It is assumed the region controller is responsible for synchronization
-// with the userlying cloud provider.
-func (p *Provisioner) reconcileSecurityGroupRules(ctx context.Context, client regionapi.ClientWithResponsesInterface, poolName string, securitygroup *regionapi.SecurityGroupRead) error {
-	log := log.FromContext(ctx)
-
-	pool, ok := p.cluster.GetWorkloadPool(poolName)
-	if !ok {
-		return fmt.Errorf("%w: pool lookup failed", ErrConsistency)
-	}
-
-	provisionedRules, err := p.getSecurityGroupRuleIDMap(ctx, client, securitygroup.Metadata.Id)
-	if err != nil {
-		return err
-	}
-
-	required := p.generateRequiredSecurityGroupRules(pool)
-
-	create, remove := p.scheduleSecurityGroupRules(provisionedRules, required)
-
-	for id := range remove.All() {
-		log.Info("deleting security group rule", "securityGroup", securitygroup.Metadata.Id, "rule", id)
-
-		if err := p.deleteSecurityGroupRule(ctx, client, securitygroup.Metadata.Id, provisionedRules[id].Metadata.Id); err != nil {
-			return err
-		}
-	}
-
-	for id := range create.All() {
-		log.Info("creating security group rule", "securityGroup", securitygroup.Metadata.Id, "rule", id)
-
-		if _, err := p.createSecurityGroupRule(ctx, client, securitygroup.Metadata.Id, required[id]); err != nil {
 			return err
 		}
 	}
