nscaledev/uni-identity

Add version endpoint

pkg/handler/handler_v2_version.go
@@ -1,0 +1,36 @@
+/*
+Copyright 2026 Nscale.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//nolint:revive
+package handler
+
+import (
+	"net/http"
+
+	coreapi "github.com/unikorn-cloud/core/pkg/openapi"
+	"github.com/unikorn-cloud/core/pkg/server/util"
+	"github.com/unikorn-cloud/identity/pkg/constants"
+)
+
+func (h *Handler) GetApiV2Version(w http.ResponseWriter, r *http.Request) {
+	result := coreapi.ServiceVersionRead{
+		Name:    constants.Application,
+		Version: constants.Version,
+	}
+
+	h.setUncacheable(w)
+	util.WriteJSONResponse(w, r, http.StatusOK, result)
+}

pkg/handler/handler_v2_version_test.go
@@ -1,0 +1,50 @@
+/*
+Copyright 2026 Nscale.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package handler_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	coreapi "github.com/unikorn-cloud/core/pkg/openapi"
+	"github.com/unikorn-cloud/identity/pkg/constants"
+	"github.com/unikorn-cloud/identity/pkg/handler"
+)
+
+func Test_Version_Get(t *testing.T) {
+	t.Parallel()
+
+	h := &handler.Handler{}
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/api/v2/version", nil)
+
+	h.GetApiV2Version(w, r)
+
+	require.Equal(t, http.StatusOK, w.Code)
+	require.Equal(t, "no-cache", w.Header().Get("Cache-Control"))
+
+	var result coreapi.ServiceVersionRead
+
+	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &result))
+	require.Equal(t, constants.Application, result.Name)
+	require.Equal(t, constants.Version, result.Version)
+}

test/api/suites/version_test.go
@@ -1,0 +1,64 @@
+//go:build integration
+// +build integration
+
+/*
+Copyright 2026 Nscale.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//nolint:revive,testpackage // dot imports and package naming standard for Ginkgo
+package suites
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/unikorn-cloud/identity/test/api"
+)
+
+var _ = Describe("Service Version", func() {
+	Context("When reading the deployed service version", func() {
+		Describe("Given a valid bearer token", func() {
+			It("should return the service name and version", func() {
+				version, err := client.GetVersion(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(version.Name).To(HavePrefix("unikorn-identity"))
+				Expect(version.Version).To(SatisfyAny(
+					Equal("0.0.0"),
+					MatchRegexp(`^v\d+\.\d+\.\d+$`),
+				))
+
+				GinkgoWriter.Printf("Identity service version: %s %s\n", version.Name, version.Version)
+			})
+		})
+
+		Describe("Given no Authorization header", func() {
+			It("should return unauthorized", func() {
+				unauthConfig := *config
+				unauthConfig.AuthToken = ""
+				unauthClient := api.NewAPIClientWithConfig(&unauthConfig)
+
+				path := unauthClient.GetEndpoints().Version()
+				resp, _, err := unauthClient.DoRequest(ctx, http.MethodGet, path, nil, 0)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(resp.StatusCode).To(Equal(http.StatusUnauthorized))
+
+				GinkgoWriter.Printf("Unauthenticated version request returned %d as expected\n", resp.StatusCode)
+			})
+		})
+	})
+})

nscaledev/uni-compute

pkg/server/handler/handler_v2_version.go
@@ -1,0 +1,36 @@
+/*
+Copyright 2026 Nscale.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//nolint:revive
+package handler
+
+import (
+	"net/http"
+
+	"github.com/unikorn-cloud/compute/pkg/constants"
+	coreapi "github.com/unikorn-cloud/core/pkg/openapi"
+	"github.com/unikorn-cloud/core/pkg/server/util"
+)
+
+func (h *Handler) GetApiV2Version(w http.ResponseWriter, r *http.Request) {
+	result := coreapi.ServiceVersionRead{
+		Name:    constants.Application,
+		Version: constants.Version,
+	}
+
+	w.Header().Add("Cache-Control", "no-cache")
+	util.WriteJSONResponse(w, r, http.StatusOK, result)
+}

pkg/server/handler/handler_v2_version_test.go
@@ -1,0 +1,50 @@
+/*
+Copyright 2026 Nscale.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package handler_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/unikorn-cloud/compute/pkg/constants"
+	"github.com/unikorn-cloud/compute/pkg/server/handler"
+	coreapi "github.com/unikorn-cloud/core/pkg/openapi"
+)
+
+func Test_Version_Get(t *testing.T) {
+	t.Parallel()
+
+	h := &handler.Handler{}
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/api/v2/version", nil)
+
+	h.GetApiV2Version(w, r)
+
+	require.Equal(t, http.StatusOK, w.Code)
+	require.Equal(t, "no-cache", w.Header().Get("Cache-Control"))
+
+	var result coreapi.ServiceVersionRead
+
+	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &result))
+	require.Equal(t, constants.Application, result.Name)
+	require.Equal(t, constants.Version, result.Version)
+}

test/api/suites/version_test.go
@@ -1,0 +1,63 @@
+//go:build integration
+
+/*
+Copyright 2026 Nscale.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//nolint:revive,testpackage // dot imports and package naming standard for Ginkgo
+package suites
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/unikorn-cloud/compute/test/api"
+)
+
+var _ = Describe("Service Version", func() {
+	Context("When reading the deployed service version", func() {
+		Describe("Given a valid bearer token", func() {
+			It("should return the service name and version", func() {
+				version, err := client.GetVersion(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(version.Name).To(HavePrefix("unikorn-compute-"))
+				Expect(version.Version).To(SatisfyAny(
+					Equal("0.0.0"),
+					MatchRegexp(`^v\d+\.\d+\.\d+$`),
+				))
+
+				GinkgoWriter.Printf("Compute service version: %s %s\n", version.Name, version.Version)
+			})
+		})
+
+		Describe("Given no Authorization header", func() {
+			It("should return unauthorized", func() {
+				unauthConfig := *config
+				unauthConfig.AuthToken = ""
+				unauthClient := api.NewAPIClientWithConfig(&unauthConfig)
+
+				path := unauthClient.GetEndpoints().Version()
+				resp, _, err := unauthClient.DoRequest(ctx, http.MethodGet, path, nil, 0)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(resp.StatusCode).To(Equal(http.StatusUnauthorized))
+
+				GinkgoWriter.Printf("Unauthenticated version request returned %d as expected\n", resp.StatusCode)
+			})
+		})
+	})
+})